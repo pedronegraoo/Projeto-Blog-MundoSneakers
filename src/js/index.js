623c00561298a3f948a856bb70d6d138ef640656
Pedro Negrao <macedopedro117@yahoo.com.br>
2023-11-19 20:09:57 -0300

    Adição da funcionalidade dos comentários

--- src/js/index.js
+++ src/js/index.js
@@ -37,7 +37,9 @@ function addComent() {
         comment: inputComment.value,
         createdAt: new Date(),
       };
+
       armazenar.push(obj);
+
       // localStorage.setItem(inputName.value, inputComment.value);
       localStorage.setItem(
         obj.nome,
@@ -45,43 +47,40 @@ function addComent() {
         obj.createdAt.toLocaleDateString()
       );
 
-      // input.removeChild(labelName);
-      // input.removeChild(inputName).value = "";
-      // input.removeChild(labelComment);
-      // input.removeChild(inputComment).value = "";
       inputName.value = "";
       inputComment.value = "";
       alert(`Comentário feito com sucesso!`);
       newComment2();
+
+      // ADICIONANDO NOVAMENTO O EVENTO DE CLICK DO +
       // btn.addEventListener("click", addComent);
     } else {
       alert(`Campo nome/comentário vazio, tente novamente`);
     }
   });
   input.append(labelName, inputName, labelComment, inputComment);
-  // btn.removeEventListener("click", addComent);
+  btn.removeEventListener("click", addComent);
 }
 
-// btnSend.addEventListener("click", function () {
-//   localStorage.setItem("Coment", inputComment.value);
-//   inputComment.value = "";
-//   // btn.addEventListener("click", addComent);
-// });
-
 const divAllComments = document.querySelector(".section2-content-top");
+
 const card1 = document.getElementById("card1");
 const card2 = document.getElementById("card2");
 const card3 = document.getElementById("card3");
+
 const nomeCard1 = document.querySelector(".nome-card1");
 const commentCard1 = document.querySelector(".comment-card1");
-const dataComment1 = document.querySelector(".dataComment1");
+const dataCommentCard1 = document.querySelector(".dataComment-card1");
+
 const nomeCard2 = document.querySelector(".nome-card2");
 const commentCard2 = document.querySelector(".comment-card2");
-const dataComment2 = document.querySelector(".dataComment2");
+const dataCommentCard2 = document.querySelector(".dataComment-card2");
+
 const nomeCard3 = document.querySelector(".nome-card3");
 const commentCard3 = document.querySelector(".comment-card3");
-const dataComment3 = document.querySelector(".dataComment3");
-let cont = 1;
+const dataCommentCard3 = document.querySelector(".dataComment-card3");
+
+// CARD SELECIONADO
 
 function newComment2() {
   const selecionado1 = document.querySelector(".card-comments.selecionado1");
@@ -91,201 +90,153 @@ function newComment2() {
   if (selecionado1) {
     card1.classList.remove("selecionado1");
     card2.classList.add("selecionado2");
-    newCommentCard1();
+    // newCommentCard1();
+    allExecute(selecionado1);
   } else if (selecionado2) {
     card2.classList.remove("selecionado2");
     card3.classList.add("selecionado3");
-    newCommentCard2();
+    // newCommentCard2();
+    allExecute(selecionado2);
   } else if (selecionado3) {
     card3.classList.remove("selecionado3");
     card1.classList.add("selecionado1");
-    newCommentCard3();
+    // newCommentCard3();
+    allExecute(selecionado3);
   }
 }
 
-function newCommentCard1() {
-  // card1.removeChild(nomeCard1);
-  // card1.removeChild(commentCard1);
-  // card1.removeChild(dataComment1);
-  // divAllComments.removeChild();
+// FUNÇÃO PARA APAGAR E INSERIR NOVOS DADOS NOS CARD'S
 
-  // não está dando certo pq quando ele cria um novo não se sabe qual posição vai ser criado os elementos do card(nome, comentario e data)
-  console.log(card1.childNodes);
-  console.log(divAllComments.childNodes);
-  console.log(divAllComments.childNodes[1]);
+function allExecute(selecionado) {
+  console.log(selecionado);
+  console.log(selecionado.id);
 
-  card1.childNodes[1].remove();
-  card1.childNodes[2].remove();
-  card1.childNodes[3].remove();
-  // divAllComments.childNodes[1].remove();
-
-  const newCard1 = document.createElement("div");
-  newCard1.id = "card1";
-  newCard1.classList = "card-comments";
-
-  const newName = document.createElement("h1");
-  newName.classList = "nome-card1";
-  newName.innerText = obj.nome;
-
-  const newComment = document.createElement("p");
-  // newComment.innerText = obj.comment;
-  newComment.classList = "comment-card1";
-  newComment.textContent = localStorage.getItem(obj.nome);
-
-  const data = document.createElement("p");
-  data.classList = "dataComment1";
-  data.innerText = `Enviado em: ${obj.createdAt.toLocaleDateString()}`;
-
-  card1.append(newName, newComment, data);
-  // divAllComments.appendChild(card1);
-  // newCard1.append(newName, newComment, data);
-  // divAllComments.appendChild(newCard1);
-}
-
-function newCommentCard2() {
-  // card2.removeChild(nomeCard2);
-  // card2.removeChild(commentCard2);
-  // card2.removeChild(dataComment2);
-  console.log(card2.childNodes[0].remove());
-  card2.childNodes[0].remove();
-  card2.childNodes[1].remove();
-  card2.childNodes[2].remove();
-
-  const newCard2 = document.createElement("div");
-  newCard2.id = "card2";
-  newCard2.classList = "card-comments";
-
-  const newName = document.createElement("h1");
-  newName.classList = "nome-card2";
-  newName.innerText = obj.nome;
-
-  const newComment = document.createElement("p");
-  // newComment.innerText = obj.comment;
-  newComment.classList = "comment-card2";
-  newComment.textContent = localStorage.getItem(obj.nome);
-
-  const data = document.createElement("p");
-  data.classList = "dataComment2";
-  data.innerText = `Enviado em: ${obj.createdAt.toLocaleDateString()}`;
-
-  card2.append(newName, newComment, data);
-  // newCard2.append(newName, newComment, data);
-  // divAllComments.appendChild(card2);
-}
-
-function newCommentCard3() {
-  // card3.removeChild(nomeCard3);
-  // card3.removeChild(commentCard3);
-  // card3.removeChild(dataComment3);
-  console.log(card3.childNodes[0].remove());
-  card3.childNodes[0].remove();
-  card3.childNodes[1].remove();
-  card3.childNodes[2].remove();
-
-  const newCard3 = document.createElement("div");
-  newCard3.id = "card3";
-  newCard3.classList = "card-comments";
-
-  const newName = document.createElement("h1");
-  newName.classList = "nome-card3";
-  newName.innerText = obj.nome;
-
-  const newComment = document.createElement("p");
-  // newComment.innerText = obj.comment;
-  newComment.classList = "comment-card3";
-  newComment.textContent = localStorage.getItem(obj.nome);
-
-  const data = document.createElement("p");
-  data.classList = "dataComment3";
-  data.innerText = `Enviado em: ${obj.createdAt.toLocaleDateString()}`;
-
-  card3.append(newName, newComment, data);
-  // newCard3.append(newName, newComment, data);
-  // divAllComments.appendChild(card3);
-}
-
-function newComment() {
-  if (cont === 1) {
-    // divAllComments.removeChild(card1);
-    // card1.removeChild(nomeCard1);
-    // card1.removeChild(commentCard1);
-    // card1.removeChild(dataComment1);
+  if (selecionado.id === "card1") {
     nomeCard1.childNodes[0].remove();
     commentCard1.childNodes[0].remove();
-
-    const newCard1 = document.createElement("div");
-    newCard1.id = "card1";
-    newCard1.classList = "card-comments";
+    dataCommentCard1.childNodes[0].remove();
 
     const newName = document.createElement("h1");
-    newName.classList = "nome-card1";
+    // newName.classList = `nome-${selecionado.id}`;
+    newName.classList = `newNameCard`;
     newName.innerText = obj.nome;
 
     const newComment = document.createElement("p");
     // newComment.innerText = obj.comment;
-    newComment.classList = "comment-card1";
+    // newComment.classList = `comment-${selecionado.id}`;
+    newComment.classList = `newCommentCard`;
     newComment.textContent = localStorage.getItem(obj.nome);
 
     const data = document.createElement("p");
-    data.classList = "dataComment1";
+    // data.classList = `dataComment-${selecionado.id}`;
+    data.classList = `newDataCard`;
     data.innerText = `Enviado em: ${obj.createdAt.toLocaleDateString()}`;
 
-    // card1.append(newName, newComment, data);
-    newCard1.append(newName, newComment, data);
-    divAllComments.appendChild(newCard1);
-
-    cont++;
-  } else if (cont === 2) {
-    card2.remove();
-
-    const newCard2 = document.createElement("div");
-    newCard2.id = "card2";
-    newCard2.classList = "card-comments";
+    nomeCard1.append(newName);
+    commentCard1.append(newComment);
+    dataCommentCard1.append(data);
+  } else if (selecionado.id === "card2") {
+    nomeCard2.childNodes[0].remove();
+    commentCard2.childNodes[0].remove();
+    dataCommentCard2.childNodes[0].remove();
 
     const newName = document.createElement("h1");
-    newName.classList = "nome-card2";
+    // newName.classList = `nome-${selecionado.id}`;
+    newName.classList = `newNameCard`;
     newName.innerText = obj.nome;
 
     const newComment = document.createElement("p");
     // newComment.innerText = obj.comment;
-    newComment.classList = "comment-card2";
+    // newComment.classList = `comment-${selecionado.id}`;
+    newComment.classList = `newCommentCard`;
     newComment.textContent = localStorage.getItem(obj.nome);
 
     const data = document.createElement("p");
-    data.classList = "dataComment2";
+    // data.classList = `dataComment-${selecionado.id}`;
+    data.classList = `newDataCard`;
     data.innerText = `Enviado em: ${obj.createdAt.toLocaleDateString()}`;
 
-    // card2.append(newName, newComment, data);
-    newCard2.append(newName, newComment, data);
-    divAllComments.appendChild(newCard2);
-
-    cont++;
-  } else if (cont === 3) {
-    card3.remove();
-
-    const newCard3 = document.createElement("div");
-    newCard3.id = "card3";
-    newCard3.classList = "card-comments";
+    nomeCard2.append(newName);
+    commentCard2.append(newComment);
+    dataCommentCard2.append(data);
+  } else if (selecionado.id === "card3") {
+    nomeCard3.childNodes[0].remove();
+    commentCard3.childNodes[0].remove();
+    dataCommentCard3.childNodes[0].remove();
 
     const newName = document.createElement("h1");
-    newName.classList = "nome-card3";
+    // newName.classList = `nome-${selecionado.id}`;
+    newName.classList = `newNameCard`;
     newName.innerText = obj.nome;
 
     const newComment = document.createElement("p");
     // newComment.innerText = obj.comment;
-    newComment.classList = "comment-card3";
+    // newComment.classList = `comment-${selecionado.id}`;
+    newComment.classList = `newCommentCard`;
     newComment.textContent = localStorage.getItem(obj.nome);
 
     const data = document.createElement("p");
-    data.classList = "dataComment3";
+    // data.classList = `dataComment-${selecionado.id}`;
+    data.classList = `newDataCard`;
     data.innerText = `Enviado em: ${obj.createdAt.toLocaleDateString()}`;
 
-    // card3.append(newName, newComment, data);
-    newCard3.append(newName, newComment, data);
-    divAllComments.appendChild(newCard3);
-
-    cont = 1;
+    nomeCard3.append(newName);
+    commentCard3.append(newComment);
+    dataCommentCard3.append(data);
   }
 }
 
-//Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, laboriosam nemo. Quos minima ad corrupti aut odio exercitationem dolorem a.
+// QUERO TESTAR DEPOIS A FUNÇÃO ABAIXO
+
+function allExecute2(selecionado) {
+  console.log(selecionado);
+  console.log(selecionado.id);
+  console.log(`nome${selecionado.id}`);
+
+  // selecionado.remove();
+
+  // selecionado.removeChild(`nome${selecionado.id}`);
+  // selecionado.removeChild(`comment${selecionado.id}`);
+  // selecionado.removeChild(`dataComment${selecionado.id}`);
+
+  if (selecionado.id === "card1") {
+    console.log(nomeCard1);
+    console.log(commentCard1);
+    console.log(dataCommentCard1);
+
+    // selecionado.removeChild(nomecard1);
+    // selecionado.removeChild(commentcard1);
+    // selecionado.removeChild(dataCommentcard1);
+
+    nomeCard1.childNodes[0].remove();
+    commentCard1.childNodes[0].remove();
+    dataCommentCard1.childNodes[0].remove();
+
+    // const newCard = document.createElement("div");
+    // newCard.id = "card1";
+    // newCard.classList = "card-comments";
+
+    const newName = document.createElement("p");
+    newName.classList = `nome-${selecionado.id}`;
+    newName.innerText = obj.nome;
+
+    const newComment = document.createElement("p");
+    // newComment.innerText = obj.comment;
+    newComment.classList = `comment-${selecionado.id}`;
+    newComment.textContent = localStorage.getItem(obj.nome);
+
+    const data = document.createElement("p");
+    data.classList = `dataComment-${selecionado.id}`;
+    data.innerText = `Enviado em: ${obj.createdAt.toLocaleDateString()}`;
+
+    nomeCard1.append(newName);
+    commentCard1.append(newComment);
+    dataCommentCard1.append(data);
+    // selecionado.append(newName, newComment, data);
+
+    // card1.append(newName, newComment, data);
+    // divAllComments.appendChild(card1);
+    // newCard1.append(newName, newComment, data);
+    // divAllComments.appendChild(newCard1);
+  }
+}
